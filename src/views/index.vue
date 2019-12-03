7d093a948873953a1ce550af9a09617e2160cd1f
cxking93 <50572433+cxking93@users.noreply.github.com>
2019-12-03 17:03:02 +0800

    Add files via upload

---
+++ src/views/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='herox'>
+    index
+  </div>
+</template>
+<script>
+export default {
+   data () {
+       return {
+       }
+   },
+   components: {
+   },
+   methods: {
+   },
+   created () {
+   }
+}
+</script>
+<style lang='less' scoped>
+.herox{
+  width: 100%;
+  background: #eee;
+  background-size: 100% 100%;
+  background-position: center;
+  height: 100vh;
+  margin: 0px;
+  padding: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 90px;
+  color: #000;
+}
+</style>
